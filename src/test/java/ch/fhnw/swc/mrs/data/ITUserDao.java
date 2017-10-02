1eac5d0ee78171db258c4bad20fe36b8b26f9955
Marco Agovino <marco.agovino@students.fhnw.ch>
2017-10-02 17:55:43 +0200

    database configuration

--- src/test/java/ch/fhnw/swc/mrs/data/ITUserDao.java
+++ src/test/java/ch/fhnw/swc/mrs/data/ITUserDao.java
@@ -43,7 +43,7 @@ public class ITUserDao extends DBTestCase {
     private Connection connection;
 
     private static final String COUNT_SQL = "SELECT COUNT(*) FROM clients";
-    private static final String DB_CONNECTION = "jdbc:hsqldb:mem:mrs";
+    private static final String DB_CONNECTION = "jdbc:hsqldb:file:testdb";
 
 	/** Create a new Integration Test object. */
 	public ITUserDao() {
